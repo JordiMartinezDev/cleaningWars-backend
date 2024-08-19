44ec2ae249095d4e7c7020a2811e87cb4331647e
JordiMartinezDev <jordiupc87@gmail.com>
2024-08-19 18:59:38 +0200

    Forced commit, rebuilt project with spring-boot initializer

---
+++ src/main/java/cleaningwars/com/cleaning_wars/CleaningWarsApplication.java
@@ -0,0 +1,13 @@
+package cleaningwars.com.cleaning_wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaningWarsApplication.class, args);
+	}
+
+}
