9fa104fada294609a2bf89d848e97a92143a2aec
JordiMartinezDev <jordiupc87@gmail.com>
2024-08-19 20:03:40 +0200

    Basic REST API without security

---
+++ src/main/java/cleaningwars/com/cleaning_wars/controllers/BasicController.java
@@ -0,0 +1,14 @@
+package cleaningwars.com.cleaning_wars.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class BasicController {
+    @GetMapping("/")
+    public String getMethodName() {
+        return "Hello";
+    }
+    
+}
