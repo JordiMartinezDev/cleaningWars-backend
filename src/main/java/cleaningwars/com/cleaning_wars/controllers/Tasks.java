353a5ba5e54a6a5e13eb20a25204c076dfa19157
JordiMartinezDev <jordiupc87@gmail.com>
2024-08-22 17:57:45 +0200

    Removed basicController + updated Tasks controller

--- src/main/java/cleaningwars/com/cleaning_wars/controllers/Tasks.java
+++ src/main/java/cleaningwars/com/cleaning_wars/controllers/Tasks.java
@@ -6,6 +6,8 @@ import cleaningwars.com.cleaning_wars.model.Task;
 import cleaningwars.com.cleaning_wars.repositories.TasksRepository;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,22 +25,11 @@ public class Tasks {
     @Autowired
     TasksRepository tasksRepository;
 
-    Task task = new Task("kitchen","baby",3);
 
-    @GetMapping("/tasks")
-    public String getMethodName(Model model) {
-        
-        Task task = new Task("Clean kitchen", "kitchen", 7);
-        model.addAttribute("task", task);
-        return "Tasks";
+    @GetMapping("/getTaskList")
+    public List<Task> getMethodName() {
+        return tasksRepository.getTaskList();
     }
-    @GetMapping("/tasks/newtask")
-    public String getMethodName(@RequestParam String name, @RequestParam String icon, @RequestParam int score) {
-        return "Created, good job";
-    }
-
-    
-    
     
     
 }
--- src/main/java/cleaningwars/com/cleaning_wars/repositories/TasksRepository.java
+++ src/main/java/cleaningwars/com/cleaning_wars/repositories/TasksRepository.java
@@ -1,6 +1,7 @@
 package cleaningwars.com.cleaning_wars.repositories;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -10,13 +11,19 @@ import cleaningwars.com.cleaning_wars.model.Task;
 @Component
 public class TasksRepository {
     
-    
-    private List<Task> allTasks = new ArrayList<>();
+    // Populating the data as we don't have DB connection yet
+    private List<Task> tasks = Arrays.asList(
+        new Task("Kitchen","kitchen",5),
+        new Task("Bathroom","toilet",4),
+        new Task("Sweep room","toilet",2),
+        new Task("Cook lunch","toilet",3)
+
+    );
 
     public List<Task> getTaskList(){
-        return allTasks;
+        return tasks;
     }
     public Task getTask(int index){
-        return allTasks.get(index);
+        return tasks.get(index);
     }
 }
