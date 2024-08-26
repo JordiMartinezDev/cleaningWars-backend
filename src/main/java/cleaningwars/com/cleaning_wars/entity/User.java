93cb34b388856738cdccc2cfd7691a75da0195d4
JordiMartinezDev <jordiupc87@gmail.com>
2024-08-26 09:31:26 +0200

    User entity updated ( JoinColumn added )

--- src/main/java/cleaningwars/com/cleaning_wars/entity/User.java
+++ src/main/java/cleaningwars/com/cleaning_wars/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,9 +21,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
-    
-    @ManyToOne
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "home_id", referencedColumnName = "id")
     private Home home;
 }
