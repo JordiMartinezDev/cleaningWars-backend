6d0f61976f4a8e0a957e0004672a5559f45c13dc
JordiMartinezDev <jordiupc87@gmail.com>
2024-08-26 09:36:09 +0200

    Home entity updated with tasks, events and user Sets

---
+++ src/main/java/cleaningwars/com/cleaning_wars/entity/Event.java
@@ -0,0 +1,41 @@
+package cleaningwars.com.cleaning_wars.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date date;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "task_id", referencedColumnName = "id")
+    private Task task;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "home_id", referencedColumnName = "id")
+    private Home home;
+}
+
--- src/main/java/cleaningwars/com/cleaning_wars/entity/Home.java
+++ src/main/java/cleaningwars/com/cleaning_wars/entity/Home.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Entity
 @Getter
@@ -15,12 +18,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Home {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    // private User[] users;
-    
-    
+
+    @OneToMany(mappedBy = "home")
+    private Set<User> users;
+
+    @OneToMany(mappedBy = "home")
+    private Set<Task> tasks;
+
+    @OneToMany(mappedBy = "home")
+    private Set<Event> events;
 }
