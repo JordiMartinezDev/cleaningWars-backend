730a15dfaa23089f86e5fbf8d0f741b22d477309
JordiMartinezDev <jordiupc87@gmail.com>
2024-08-27 07:45:03 +0200

    Event service implementation

--- src/main/java/cleaningwars/com/cleaning_wars/services/EventServiceImplementation.java
+++ src/main/java/cleaningwars/com/cleaning_wars/services/EventServiceImplementation.java
@@ -1,5 +1,35 @@
 package cleaningwars.com.cleaning_wars.services;
 
-public class EventServiceImplementation {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import cleaningwars.com.cleaning_wars.entity.Event;
+import cleaningwars.com.cleaning_wars.repositories.EventRepository;
+
+import java.util.List;
+
+@Service
+public class EventServiceImplementation implements EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Override
+    public Event addEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public Event getEventById(Long id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public void deleteEvent(Long id) {
+        eventRepository.deleteById(id);
+    }
 }
