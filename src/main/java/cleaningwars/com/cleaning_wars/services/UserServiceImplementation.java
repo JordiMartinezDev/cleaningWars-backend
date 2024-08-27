6c9459e5f8b481bc2ad7ef6fe3447acad6a1e175
JordiMartinezDev <jordiupc87@gmail.com>
2024-08-27 07:48:03 +0200

    User service implementation updated

--- src/main/java/cleaningwars/com/cleaning_wars/services/UserServiceImplementation.java
+++ src/main/java/cleaningwars/com/cleaning_wars/services/UserServiceImplementation.java
@@ -1,5 +1,39 @@
 package cleaningwars.com.cleaning_wars.services;
 
-public class UserServiceImplementation {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import cleaningwars.com.cleaning_wars.entity.User;
+import cleaningwars.com.cleaning_wars.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImplementation implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void updateUser(Long id, User user){
+        
+    }
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
